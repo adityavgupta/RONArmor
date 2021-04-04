bf2eb6bc5c272077ca1e626a92f7afc5d154a49f
Aditya Gupta <aditya@aditya.local>
2021-04-04 14:41:19 -0500

    basic pir led test

--- pir.py
+++ pir.py
@@ -1,10 +1,24 @@
 from machine import Pin
-import time
+from time import sleep
 
-ldr = Pin(0, Pin.IN)  # create input pin on GPIO2
+motion = False
+
+def handle_interrupt(pin):
+  global motion
+  motion = True
+  global interrupt_pin
+  interrupt_pin = pin 
+
+led = Pin(12, Pin.OUT)
+pir = Pin(14, Pin.IN)
+
+pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+
 while True:
-    if ldr.value():
-        print('Object Detected')      # get value, 0 or 1
-    else:
-        print('All clear')
-	time.sleep(1)
+  if motion:
+    print('Motion detected! Interrupt caused by:', interrupt_pin)
+    led.value(1)
+    sleep(20)
+    led.value(0)
+    print('Motion stopped!')
+    motion = False
