6f234c0b5d3056ba50d2625eeab68ac2a2f048a2
Aditya Gupta <aditya@aditya.local>
2021-04-09 09:58:06 -0500

    4 pin interrupt based motion sensing code

--- pir.py
+++ pir.py
@@ -1,42 +1,41 @@
+# Name: pir.py
+# Functionality: Controls the PIR sensors based on the interrupt recieved 
 from machine import Pin
 from time import sleep
 
-# motion = pir.value()
+motion = False
+LED_PIN_1 = 23
 
-# def handle_interrupt(pin):
-#   global motion
-#   motion = True
-#   global interrupt_pin
-#   interrupt_pin = pin 
+PIR_PIN_1 = 12
+PIR_PIN_2 = 13
+PIR_PIN_3 = 14
+PIR_PIN_4 = 15
 
-led = Pin(23, Pin.OUT)
-pir = Pin(5, Pin.IN)
+def handle_interrupt(pin):
+  global motion
+  motion = True
+  global interrupt_pin
+  interrupt_pin = pin 
 
-# pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+led = Pin(LED_PIN_1, Pin.OUT)
+pir1 = Pin(PIR_PIN_1, Pin.IN)
+pir2 = Pin(PIR_PIN_2, Pin.IN)
+pir3 = Pin(PIR_PIN_3, Pin.IN)
+pir4 = Pin(PIR_PIN_4, Pin.IN)
 
-# while True:
-#   if motion:
-#     print('Motion detected! Interrupt caused by:', interrupt_pin)
-#     led.value(1)
-#     sleep(5)
-#     led.value(0)
-#     print('Motion stopped!')
-#     motion = False
+pir1.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+pir2.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+pir3.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+pir4.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
 
-
-old_value = pir.value()
-while True:
-  pir_value = pir.value()
-  if pir_value:
-    # PIR is detecting movement! Turn on LED. 
-    led.value(1)
-    # Check if this is the first time movement was # detected and print a message!
-    if not old_value:
-      print('Motion detected!') 
-  else:
-    # PIR is not detecting movement. Turn off LED. 
-    led.value(0)
-    # # Again check if this is the first time movement # stopped and print a message.
-    if old_value:
-      print('Motion ended!') 
-  old_value = pir_value
+try:
+  while True:
+    if motion:
+      print('Motion detected! Interrupt caused by:', interrupt_pin)
+      led.value(1)
+      sleep(1)
+      led.value(0)
+      print('Motion stopped!')
+      motion = False
+except:
+  print('nothing')
